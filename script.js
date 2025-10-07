f7b1a5caea978d7c792b488447372ab1ce8f225d
cysho <skotlandersv1@gmail.com>
2025-10-07 13:01:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-//1.Pilih tombol berdasarkan ID-nya
-cons tombolSapa = document.getElementByld('sapaButton');
-//2. Tambahkan 'evet listener' untuk aksi 'click'
+const tombolSapa=document.getElementByld('sapaButton');
 tombolSapa.addEventListener('click', function(){
-  //3. jalankan kode ini ketika tombol di-klik
   alert('Halo! Terimakasih sudah berkunjung');
 });
